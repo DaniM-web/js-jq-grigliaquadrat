621f161f8ffda91e5c61a8d8a835241d5e5d2b41
Danilo <danilo.maida95@libero.it>
2020-04-05 20:02:16 +0200

    consegna EX

--- script/main.js
+++ script/main.js
@@ -9,21 +9,20 @@ var square;
 var contenitore = $('.container');
 var nmrRandom;
 var assigned = [];
-var nebo = false;
-var indiceSquare;
+var contatore = 0;
+
 //creo 64 quadrati nel mio html
-
-
 for (var i = 0; i < 64; i++) {
 
-  contenitore.append('<div class="square"></div>')
+  contenitore.append('<div class="square"></div>');
   square = $('.square');
+
 }
 
 // creo un array al cui interno assegno 15 numeri random da 1 a 64
   for (var i = assigned.length; assigned.length < 15; i++) {
     nmrRandom = Math.floor(Math.random() * 64 )+ 1;
-    numeroGenerato = assigned.indexOf(nmrRandom);
+    var numeroGenerato = assigned.indexOf(nmrRandom);
  //condizione per non far ripetere i numeri uguali
     console.log("l'indice del numero random è " + numeroGenerato );
     if (numeroGenerato === -1) {
@@ -46,23 +45,25 @@ $('.restart').click(
 function squareClicked() {
   //ciclo che assegna lo stile red a 15 div random, corrispondenti ai 15 numeri dell'array
   for (var i = 0; i < assigned.length; i++) {
-    indiceSquare = assigned[i]-1;
+    var indiceSquare = assigned[i]-1;
     $('.square').eq(indiceSquare).addClass('red');
   }
-
+  // se il quadrato ha la classe rossa, allora
   if($(this).hasClass('red')){
     $(this).addClass('active');
-    square.addClass('active green')+
-    $('.restart').fadeIn('slow');
+    $('.red').addClass('bomba active');
+    square.not('.red').addClass('green fiore');
 
+    $('.losepage').fadeIn(5000);
+    $('button.restart').slideDown(6000);
+    //se non ha la classe rossa
   }else {
-    $(this).addClass('green');
+    $(this).addClass('green fiore');
   }
 
 }
 //funzione per resettare il gioco
 function submit() {
-  $('.restart').fadeOut('slow');
   location.reload(true);
 }
 
